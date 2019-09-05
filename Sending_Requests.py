9b3e00bc9b5ccdb0ce86035673161b828ddec06d
Maheshwar Kuchana <kmaheshwar1998@gmail.com>
2019-09-05 11:16:32 +0530

    1

--- Sending_Requests.py
+++
@@ -1,6 +0,0 @@
-def Send_Request(Person, Button, n):
-    n = n+1
-    li_button_num = Button+11   #7 for me
-    li_button = Person.find_element_by_id("ember"+str(li_button_num))
-    li_button.click()
-    return n
